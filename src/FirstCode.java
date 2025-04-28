d4b0b171b40a516c9346a589f7b385a61d732736
mayursinkar <mayursinkar@gmail.com>
2025-04-28 20:38:51 +0530

    my first code checked in

---
+++ src/FirstCode.java
@@ -0,0 +1,32 @@
+
+public class Firstcode {
+
+	int age = 6;
+	String name ="Raghav";
+	
+
+	
+	int add1 = 25;
+	int add2 =35;
+	double sub =7.5;
+	float div =3;
+	int mult= 4;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+
+	Firstcode f	=new Firstcode();
+	System.out.println(f.age);
+	System.out.println(f.name);
+	
+	System.out.println (f.add1+f.add2);
+	System.out.println(f.add1-f.sub);
+	System.out.println(f.add1*f.mult);
+	System.out.println(f.sub/f.div);
+
+	
+	}
+
+}
